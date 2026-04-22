7152a7532ef533fe3bce282b04c9459a9ae54b2a
terfiel-22 <dietherfiel@gmail.com>
2026-04-22 17:35:50 +0800

    Autoloading (PSR-4) & Composer

--- src/public/index.php
+++ src/public/index.php
@@ -2,10 +2,7 @@
 
 use App\PaymentGateway\Paddle\Transaction;
 
-spl_autoload_register(function ($class) {
-    $path = __DIR__ . "/../" . lcfirst(str_replace("\\", "/", $class)) . ".php";
-    require $path;
-});
+require __DIR__ . "/../vendor/autoload.php";
 
 $transaction = (new Transaction(100, "Transaction"))
     ->addTax(8)
